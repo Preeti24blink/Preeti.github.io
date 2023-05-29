86a066ed80ae2ccc6ff5598b1cdb880e921d4265
Preeti24blink <134396519+Preeti24blink@users.noreply.github.com>
2023-05-29 21:47:29 +0530

    Add files via upload

---
+++ CA1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CA1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List l=new ArrayList();
+		l.add("IIMT");
+		l.add(100);
+		l.add(true);
+		l.add("IIMT");
+		System.out.println(l);
+
+	}
+
+}
